4b7291e648c6a0c9493d697b0c3fbb1056515186
Caden Dinkel <cdink@Cadens-MacBook-Pro.local>
2026-06-30 23:39:53 -0500

    Adding helix package and home-manager input.

--- flake.nix
+++ flake.nix
@@ -5,15 +5,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [ pkgs.vim pkgs.helix
         ];
 
       # Necessary for using flakes on this system.
