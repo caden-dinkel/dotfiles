644c6ef1954add4011b731c88d6f820d2223c450
Caden Dinkel <cdink@Cadens-MacBook-Pro.local>
2026-07-01 00:04:48 -0500

    Adding  to Caden's-MacBook-Pro darwin config in flake.nix

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Cadens-MacBook-Pro
     darwinConfigurations."Cadens-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      inherit inputs;
       modules = [ ./darwin/configuration.nix ];
     };
   };
