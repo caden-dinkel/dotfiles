864d3cb7893aaa1d701973a816fec451593ee40f
Caden Dinkel <cdink@Cadens-MacBook-Pro.local>
2026-07-02 14:17:57 -0500

    Setting allowUnfree to .

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,8 @@
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
+  nixpkgs.config.allowUnfree = true;
+
   # Enable alternative shell support in nix-darwin.
   # programs.fish.enable = true;
 
