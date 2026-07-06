39e6f054dbaf56dacf8c3c5f0a2a942e449ee7da
Caden Dinkel <cdink@Cadens-MacBook-Pro.local>
2026-07-05 22:07:19 -0500

    Adding yabai service to configuration.nix.

--- configuration.nix
+++ configuration.nix
@@ -18,10 +18,57 @@
   # Set Git commit hash for darwin-version.
   system.configurationRevision = self.rev or self.dirtyRev or null;
 
+  system.defaults = {
+    finder = {
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+    };
+    NSGlobalDomain = {
+      AppleShowAllFiles = true;
+      _HIHideMenuBar = true;
+    };
+    dock.autohide = true;
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.fira-mono
+    nerd-fonts.hack
+  ];
+  
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 6;
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
+
+  services.yabai = {
+    enable = true;
+    package = pkgs.yabai;
+    enableScriptingAddition = false;
+    config = {
+      layout = "bsp";
+      focus_follows_mouse = "autoraise";
+      window_placement = "second_child";
+      window_opacity = "off";
+      top_padding = 10;
+      bottom_padding = 10;
+      left_padding = 10;
+      right_padding = 10;
+      window_gap = 10;
+    };
+    extraConfig = ''
+      yabai -m rule --add app="^System Settings$" manage=off
+    '';
+  };
+
+  services.skhd = {
+    enable = true;
+    package = pkgs.skhd;
+
+    skhdConfig = ''
+      cmd - return : open -na "/Users/cadend/Applications/Home Manager Apps/WezTerm.app"
+    '';
+  };
 }
