353246d2856463732002b02cfa94411e6e4be206
Caden Dinkel <git@cdink.dev>
2026-08-19 16:03:25 -0500

    Adding comment to show possible correct deploy user.

--- users/deploy.nix
+++ users/deploy.nix
@@ -3,9 +3,12 @@
     users.users.deploy = {
         description = "Deployment User for deploy-rs";
         group = "deploy";
-        # Check whether deploy-rs/SSH need a home dir. 
-        createHome = false;
+        # SSH requires a home-dir.
+        createHome = true;
+        home = "/var/lib/deploy";
+        shell = pkgs.bashInteractive; 
         isSystemUser = true;
+        
         openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQSQpXpm7lmsfqUnyUzRD+h3CkXVlxPKyhMOSxAa7ml mac@cdink.dev"
         ];
@@ -32,3 +35,41 @@
       ];
   };
 }
+
+
+
+
+# Found on Reddit. Might be decent, but it's a year old.
+/*
+  security.sudo.extraRules = [
+    {
+      groups = ["deploy"];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/systemd-run";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/nix/store/*/bin/switch-to-configuration";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-store";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-env";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = ''/bin/sh -c "readlink -e /nix/var/nix/profiles/system || readlink -e /run/current-system"'';
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-collect-garbage";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+*/
