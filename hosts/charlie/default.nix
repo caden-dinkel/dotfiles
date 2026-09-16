3825a02485eb09847e4dd0b15878fd8df60f5450
Caden Dinkel <git@cdink.dev>
2026-09-16 20:11:11 +0000

    nix fmt

--- hosts/charlie/default.nix
+++ hosts/charlie/default.nix
@@ -17,7 +17,7 @@
   };
   disko.devices.disk.primary.device = "/dev/disk/by-id/nvme-eui.002538b971031fda";
   disko.devices.disk.secondary.device = "/dev/disk/by-id/wwn-0x5000cca8d8ec939a";
-  
+
   services.nix-serve = {
     enable = true;
     package = pkgs.nix-serve-ng;
