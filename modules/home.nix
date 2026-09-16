eb27ca2bc38b5375eb8745f933d6db98d4c8a578
Caden Dinkel <git@cdink.dev>
2026-09-16 04:14:51 +0000

    Adding firefox and bitwarden

--- modules/home.nix
+++ modules/home.nix
@@ -7,6 +7,7 @@
             pkgs.tree
             pkgs.tmux
             pkgs.ripgrep
+            pkgs.bitwarden-desktop
         ];
         home.homeDirectory = if pkgs.stdenv.hostPlatform.isLinux then "/home/${name}" else "/Users/${name}";
         home.stateVersion = "26.05";
@@ -47,6 +48,9 @@
             enable = true;
         };
         programs.obsidian = {
+            enable = true;
+        };
+        programs.firefox = {
             enable = true;
         };
     };
