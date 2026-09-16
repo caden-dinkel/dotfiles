8934a4d6ed421a6ac428028b9162e4c79810db23
Caden Dinkel <git@cdink.dev>
2026-09-16 10:00:22 -0500

    Adding rustfmt and prettier.

--- modules/treefmt.nix
+++ modules/treefmt.nix
@@ -3,9 +3,7 @@
   programs = {
     # Enable nixpkgsfmt or alejandra for Nix files
     alejandra.enable = true;
-
-    # Add other formatters easily, e.g.:
-    # prettier.enable = true; # for JS/TS/JSON/Markdown
-    # rustfmt.enable = true;  # for Rust
+    rustfmt.enable = true;
+    prettier.enable = true; # for JS/TS/JSON/Markdown
   };
 }
