99cda893691857bfab2f03d685a7439f7e08ba04
Caden Dinkel <git@cdink.dev>
2026-09-16 21:20:23 -0500

    Removing extra formatters from nixfmt

--- modules/treefmt.nix
+++ modules/treefmt.nix
@@ -3,7 +3,5 @@
   programs = {
     # Enable nixpkgsfmt or alejandra for Nix files
     nixfmt.enable = true;
-    rustfmt.enable = true;
-    prettier.enable = true; # for JS/TS/JSON/Markdown
   };
 }
