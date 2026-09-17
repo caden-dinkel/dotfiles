2ae315380aca1bc19e827aff92b9e26fe9ea4b8d
Caden Dinkel <git@cdink.dev>
2026-09-17 14:32:59 -0500

    Adjusting darwin version

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
