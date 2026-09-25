4bd001d6134f4f337898469e0f9cfbf0f35ecb64
Caden Dinkel <git@cdink.dev>
2026-09-25 18:35:09 -0500

    Add ll?

--- modules/home.nix
+++ modules/home.nix
@@ -8,7 +8,10 @@
 }:
 let
   myHomeDir = if pkgs.stdenv.hostPlatform.isLinux then "/home/${name}" else "/Users/${name}";
-  zshContent = ''alias ll="ls -alF"'' + (
+  zshContent = ''
+    alias ll="ls -alF"
+  ''
+  + (
     if pkgs.stdenv.hostPlatform.isLinux then
       ''
         alias rebuild="sudo nixos-rebuild switch --flake .#${config.networking.hostName}"
@@ -17,7 +20,7 @@ let
       ''
         alias rebuild="sudo darwin-rebuild switch --flake .#${config.networking.hostName}"
       ''
-        );
+  );
 in
 {
   home-manager.useGlobalPkgs = true;
